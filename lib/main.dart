b80b0b607438fe82b0017da72610aded9262e473
神谷 <cinesuija@gmail.com>
2021-05-01 12:25:30 +0900

    update: 商品のリストを作成する

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,39 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("SUZURI"),
       ),
-      body: Center(
-        child: Text("Hello, SUZURI!"),
-      ),
+      body: _productsList(context), // bodyで表示したいウィジェットを別のメソッドに切り出す
     );
   }
+}
+
+// Widgetを返すメソッド
+// 引数はBuildContextで、呼び出し側のbuildで持っているものを渡す
+Widget _productsList(BuildContext context) {
+  return Container(
+    // GridViewはウィジェットをグリッドで表示してくれるウィジェット
+    // iOS UIKitで言うところの UICollectionView
+    // GridView.builderというfactory(カスタムコンストラクタ)で初期化する
+    child: GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        // グリッド横方向のウィジェット数
+        crossAxisCount: 2,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
+        // グリッド表示するウィジェットの縦横比
+        childAspectRatio: 0.7,
+      ),
+      // グリッドに表示したいウィジェットの数
+      itemCount: 6,
+      // itemBuilderはGridViewのインデックス毎に表示したいウィジェットを返すデリゲート
+      // context, indexを引数にとり、ウィジェットを返す関数を指定してやる
+      // itemContの回数だけ呼ばれる、この例では6回
+      itemBuilder: (context, index) {
+        // とりあえずグレーのコンテナを表示してみる
+        return Container(
+          color: Colors.grey,
+          margin: EdgeInsets.all(16),
+        );
+      }
+    ),
+  );
 }
